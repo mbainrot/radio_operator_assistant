6216993bd1bd8b85096e94c4c9376498095af559
Max <mbainrot@gmail.com>
2014-02-08 11:59:34 +1100

    - added my very very very prototype script

---
+++ roa.pl
@@ -0,0 +1,98 @@
+#!/usr/bin/perl
+use Curses::UI;
+
+# create a new C::UI object
+my $cui = Curses::UI->new( -clear_on_exit => 1,
+                       -debug => $debug,, -color_support => 1 );
+
+# Setup the messy code
+my @menu = (
+    {
+        -label => 'File',
+        -submenu => [
+            { -label => 'New Formal Message ^F', -value => \&formal_msg   },
+            { -label => 'Exit               ^Q', -value => \&exit_dialog  }
+        ]
+    },
+
+);
+
+sub exit_dialog()
+{
+        my $return = $cui->dialog(
+                -message   => "Do you really want to quit?",
+                -title     => "Are you sure???",
+                -buttons   => ['yes', 'no'],
+
+        );
+
+exit(0) if $return;
+}
+
+my $menu = $cui->add(
+                'menu','Menubar',
+                -menu => \@menu,
+                -fg  => "blue",
+
+        );
+
+my $win1 = $cui->add(
+             'win1', 'Window',
+             -border => 1,
+         -height => 14,
+             -y    => 1,
+             -bfg  => 'red',
+
+        
+     );
+my $win2 = $cui->add(
+             'win2', 'Window',
+             -title => 'Previous Log Entries',
+         -border => 1,
+             -y    => 15,
+
+             -bfg  => 'blue',
+         #-centered => 1,
+     );
+
+my $listbox2 = $win1->add(
+        'mylistbox', 'Listbox',
+    -y => 0,
+        -values    => [1, 2, 3, 4, 99],
+        -labels    => {
+        1 => 'New Formal Message',
+                2 => 'New Informal Message/Log',
+                3 => 'New Unit Movement Record',
+        4 => 'Blah',
+        99 => 'Quit',
+    },
+    -onchange => \&menu_select,
+        # -radio     => 1,
+    );
+my $listbox = $win2->add(
+        'mylistbox', 'Listbox',
+        -values    => [1, 2, 3],
+        -labels    => { 1 => 'One',
+                        2 => 'Two',
+                        3 => 'Three' },
+        # -radio     => 1,
+    );
+
+sub menu_select()
+{
+    $sel_id = $listbox2->get();
+    if($sel_id eq 99)
+    {
+        exit_dialog();
+    }   
+}
+
+$cui->set_binding(sub {$menu->focus()}, "\cX");
+$cui->set_binding( \&exit_dialog , "\cQ");
+
+ $listbox2->focus();
+ #$textentry->focus();
+        $cui->mainloop();
+
+# start the event loop
+$cui->mainloop;
